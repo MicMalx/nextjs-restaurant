335a9e341e9966e7b8fa41256c96268844f0c719
xxx <mic.malecki95@gmail.com>
2024-05-01 10:29:21 +0200

    add /menu route

---
+++ components/MealType.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+type Props = {
+    url: string;
+    imgSrc: string;
+    label: string;
+};
+
+export default function MealType({ url, imgSrc, label }: Props) {
+    return (
+        <Link href={url} className='hover:text-[#f2c666]'>
+            <Image
+                src={imgSrc}
+                height={178}
+                width={240}
+                alt='menu part image'
+            />
+            <div className='mt-4 self-center text-center font-bold'>{label}</div>
+        </Link>
+    );
+}
