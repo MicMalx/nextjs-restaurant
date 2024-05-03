6b60be432eeb01daab025a7bcd958159e9e9e70d
xxx <mic.malecki95@gmail.com>
2024-05-03 09:38:06 +0200

    add /contact route

---
+++ app/contact/page.tsx
@@ -0,0 +1,26 @@
+export default function Contact() {
+    return (
+        <main className='text-center'>
+            <section className='text-lg'>
+                <h2 className='text-2xl text-[#B47C03] mt-5 mb-2.5'>We invite our guests:</h2>
+                <p>Monday - Wednesday: 10AM - 10PM</p>
+                <p>Thursday - Saturday: 10AM - 11PM</p>
+                <p>Sunday: 10AM - 10PM</p>
+            </section>
+            <section className='text-lg'>
+                <h2 className='text-2xl text-[#B47C03] mt-5 mb-2.5'>Address: </h2>
+                <p>
+                    129A Pritchard's Rd,
+                    <br />
+                    London E2 9AP
+                </p>
+            </section>
+            <section className='text-lg'>
+                <h2 className='text-2xl text-[#B47C03] mt-5 mb-2.5'>Contact: </h2>
+                <p>Phone Number: 682 734 991 - Restaurant</p>
+                <p>Phone Number: 661 571 373 - Manager</p>
+                <p>E-mail: restaurant@gmail.com</p>
+            </section>
+        </main>
+    );
+}
